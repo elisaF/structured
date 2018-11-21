f6d7e33b6763d242094e27b656a6b22037b243b0
Elisa Ferracane <elisa@ferracane.com>
2018-11-21 03:00:13 -0600

    more changes for children-only percolation

--- models.py
+++ models.py
@@ -230,16 +230,14 @@ class StructureModel():
             self.str_scores = str_scores  # shape is [batch_size, doc_l, doc_l+1]
 
             sents_children = tf.matmul(str_scores_no_root, sents_sem)
-            sents_sem_root = tf.concat([tf.tile(embeddings_root, [batch_l, 1, 1]), sents_sem], 1)
-            sents_parents = tf.matmul(str_scores, sents_sem_root)
             if self.config.tree_percolation == "child":
                 sents_output = LReLu(tf.tensordot(tf.concat([sents_sem, sents_children], 2), w_comb, [[2], [0]]) + b_comb)
-            elif self.config.tree_percolation == "parent":
-                sents_output = LReLu(tf.tensordot(tf.concat([sents_sem, sents_parents], 2), w_comb, [[2], [0]]) + b_comb)
-            elif self.config.tree_percolation == "both":
-                if self.config.tree_percolation_levels > 0:
-                    sents_output = LReLu(tf.tensordot(tf.concat([sents_sem, sents_children], 2), w_comb, [[2], [0]]) + b_comb)
-                else:
+            else:
+                sents_sem_root = tf.concat([tf.tile(embeddings_root, [batch_l, 1, 1]), sents_sem], 1)
+                sents_parents = tf.matmul(str_scores, sents_sem_root)
+                if self.config.tree_percolation == "parent":
+                    sents_output = LReLu(tf.tensordot(tf.concat([sents_sem, sents_parents], 2), w_comb, [[2], [0]]) + b_comb)
+                elif self.config.tree_percolation == "both":
                     sents_output = LReLu(tf.tensordot(tf.concat([sents_sem, sents_parents, sents_children], 2), w_comb_both, [[2], [0]]) + b_comb)
 
             # percolation is only supported for "children" option
