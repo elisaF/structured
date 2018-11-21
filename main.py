755012b7743ac11fdf4f98a0abdbd646476da33e
Elisa Ferracane <elisa@ferracane.com>
2018-11-21 07:28:30 -0600

    fix print

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def run(config):
 
     if config.model_dir:
         print(config)
-        print("Tensorflow version: ", print(tf.__version__))
+        print("Tensorflow version: ", tf.__version__)
         print("Git version: ", get_git_revision_hash())
         logger.critical(str(config))
         logger.critical(get_git_revision_hash())
