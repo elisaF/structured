fix masking, change from float32 to float64

main.py
@@ -91,7 +91,7 @@ def run(config):
         with tf.Session() as sess:
             gvi = tf.global_variables_initializer()
             sess.run(gvi)
-            sess.run(model.embeddings.assign(embedding_matrix.astype(np.float32)))
+            sess.run(model.embeddings.assign(embedding_matrix.astype(np.float64)))
             if config.debug:
                 sess = tf_debug.LocalCLIDebugWrapperSession(sess)
             loss = 0
@@ -136,7 +136,7 @@ def save_model(sess, step, model, logger, config):
 
     output = tf.saved_model.utils.build_tensor_info(model.final_output)
     if config.skip_doc_attention:
-        str_scores = tf.saved_model.utils.build_tensor_info(tf.convert_to_tensor(np.empty([1,1]), np.float32))
+        str_scores = tf.saved_model.utils.build_tensor_info(tf.convert_to_tensor(np.empty([1,1]), np.float64))
     else:
         str_scores = tf.saved_model.utils.build_tensor_info(model.str_scores)
     prediction_signature = (

models.py
@@ -10,7 +10,7 @@ class StructureModel():
         self.config = config
         self.xavier_init = xavier_init
         t_variables = {}
-        t_variables['keep_prob'] = tf.placeholder(tf.float32)
+        t_variables['keep_prob'] = tf.placeholder(tf.float64)
         t_variables['batch_l'] = tf.placeholder(tf.int32)
         t_variables['token_idxs'] = tf.placeholder(tf.int32, [None, None, None])
         t_variables['sent_l'] = tf.placeholder(tf.int32, [None, None])
@@ -19,10 +19,10 @@ class StructureModel():
         t_variables['max_doc_l'] = tf.placeholder(tf.int32)
         t_variables['gold_labels'] = tf.placeholder(tf.int32, [None])
         t_variables['doc_ids'] = tf.placeholder(tf.int32, [None])
-        t_variables['mask_tokens'] = tf.placeholder(tf.float32, [None, None, None])
-        t_variables['mask_sents'] = tf.placeholder(tf.float32, [None, None])
-        t_variables['mask_parser_1'] = tf.placeholder(tf.float32, [None, None, None])
-        t_variables['mask_parser_2'] = tf.placeholder(tf.float32, [None, None, None])
+        t_variables['mask_tokens'] = tf.placeholder(tf.float64, [None, None, None])
+        t_variables['mask_sents'] = tf.placeholder(tf.float64, [None, None])
+        t_variables['mask_parser_1'] = tf.placeholder(tf.float64, [None, None, None])
+        t_variables['mask_parser_2'] = tf.placeholder(tf.float64, [None, None, None])
         self.t_variables = t_variables
 
     def get_feed_dict(self, batch):
@@ -37,8 +37,8 @@ class StructureModel():
         sent_l_matrix = np.zeros([batch_size, max_doc_l], np.int32)
         gold_matrix = np.zeros([batch_size], np.int32)
         id_matrix = np.zeros([batch_size], np.int32)
-        mask_tokens_matrix = np.ones([batch_size, max_doc_l, max_sent_l], np.float32)
-        mask_sents_matrix = np.ones([batch_size, max_doc_l], np.float32)
+        mask_tokens_matrix = np.ones([batch_size, max_doc_l, max_sent_l], np.float64)
+        mask_sents_matrix = np.ones([batch_size, max_doc_l], np.float64)
         for i, instance in enumerate(batch):
             n_sents = len(instance.token_idxs)
             gold_matrix[i] = instance.goldLabel
@@ -48,8 +48,8 @@ class StructureModel():
                 mask_tokens_matrix[i, j, len(sent):] = 0
                 sent_l_matrix[i, j] = len(sent)
             mask_sents_matrix[i, n_sents:] = 0
-        mask_parser_1 = np.ones([batch_size, max_doc_l, max_doc_l], np.float32)
-        mask_parser_2 = np.ones([batch_size, max_doc_l, max_doc_l], np.float32)
+        mask_parser_1 = np.ones([batch_size, max_doc_l, max_doc_l], np.float64)
+        mask_parser_2 = np.ones([batch_size, max_doc_l, max_doc_l], np.float64)
         mask_parser_1[:, :, 0] = 0  # zero out 1st column for each doc
         mask_parser_2[:, 0, :] = 0  # zero out 1st row for each doc
         if self.config.large_data:
@@ -67,64 +67,64 @@ class StructureModel():
 
     def build(self):
         with tf.variable_scope("Embeddings"):
-            self.embeddings = tf.get_variable("emb", [self.config.n_embed, self.config.d_embed], dtype=tf.float32,
+            self.embeddings = tf.get_variable("emb", [self.config.n_embed, self.config.d_embed], dtype=tf.float64,
                                          initializer=self.xavier_init)
-            embeddings_root = tf.get_variable("emb_root", [1, 1, 2 * self.config.dim_sem], dtype=tf.float32,
+            embeddings_root = tf.get_variable("emb_root", [1, 1, 2 * self.config.dim_sem], dtype=tf.float64,
                                                   initializer=self.xavier_init)
-            embeddings_root_s = tf.get_variable("emb_root_s", [1, 1,2* self.config.dim_sem], dtype=tf.float32,
+            embeddings_root_s = tf.get_variable("emb_root_s", [1, 1,2* self.config.dim_sem], dtype=tf.float64,
                                                     initializer=self.xavier_init)
         with tf.variable_scope("Model"):
-            w_comb = tf.get_variable("w_comb", [4 * self.config.dim_sem, 2 * self.config.dim_sem], dtype=tf.float32,
+            w_comb = tf.get_variable("w_comb", [4 * self.config.dim_sem, 2 * self.config.dim_sem], dtype=tf.float64,
                             initializer=self.xavier_init)
-            w_comb_both = tf.get_variable("w_comb_both", [6 * self.config.dim_sem, 2 * self.config.dim_sem], dtype=tf.float32,
+            w_comb_both = tf.get_variable("w_comb_both", [6 * self.config.dim_sem, 2 * self.config.dim_sem], dtype=tf.float64,
                                      initializer=self.xavier_init)
-            b_comb = tf.get_variable("bias_comb", [2 * self.config.dim_sem], dtype=tf.float32, initializer=tf.constant_initializer())
+            b_comb = tf.get_variable("bias_comb", [2 * self.config.dim_sem], dtype=tf.float64, initializer=tf.constant_initializer())
 
-            w_comb_s = tf.get_variable("w_comb_s", [4 * self.config.dim_sem, 2 * self.config.dim_sem], dtype=tf.float32,
+            w_comb_s = tf.get_variable("w_comb_s", [4 * self.config.dim_sem, 2 * self.config.dim_sem], dtype=tf.float64,
                             initializer=self.xavier_init)
-            b_comb_s = tf.get_variable("bias_comb_s", [2 * self.config.dim_sem], dtype=tf.float32, initializer=tf.constant_initializer())
+            b_comb_s = tf.get_variable("bias_comb_s", [2 * self.config.dim_sem], dtype=tf.float64, initializer=tf.constant_initializer())
 
-            w_softmax = tf.get_variable("w_softmax", [2 * self.config.dim_sem, self.config.dim_output], dtype=tf.float32,
+            w_softmax = tf.get_variable("w_softmax", [2 * self.config.dim_sem, self.config.dim_output], dtype=tf.float64,
                             initializer=self.xavier_init)
-            b_softmax = tf.get_variable("bias_softmax", [self.config.dim_output], dtype=tf.float32,
+            b_softmax = tf.get_variable("bias_softmax", [self.config.dim_output], dtype=tf.float64,
                             initializer=self.xavier_init)
 
-            w_sem_doc = tf.get_variable("w_sem_doc", [2 * self.config.dim_sem, 2 * self.config.dim_sem], dtype=tf.float32,
+            w_sem_doc = tf.get_variable("w_sem_doc", [2 * self.config.dim_sem, 2 * self.config.dim_sem], dtype=tf.float64,
                                         initializer=self.xavier_init)
 
-            w_str_doc = tf.get_variable("w_str_doc", [2 * self.config.dim_sem, 2 * self.config.dim_str], dtype=tf.float32,
+            w_str_doc = tf.get_variable("w_str_doc", [2 * self.config.dim_sem, 2 * self.config.dim_str], dtype=tf.float64,
                                         initializer=self.xavier_init)
 
         with tf.variable_scope("Structure/doc"):
             tf.get_variable("w_parser_p", [2 * self.config.dim_str, 2 * self.config.dim_str],
-                            dtype=tf.float32,
+                            dtype=tf.float64,
                             initializer=self.xavier_init)
             tf.get_variable("w_parser_c", [2 * self.config.dim_str, 2 * self.config.dim_str],
-                            dtype=tf.float32,
+                            dtype=tf.float64,
                             initializer=self.xavier_init)
-            tf.get_variable("w_parser_s", [2 * self.config.dim_str, 2 * self.config.dim_str], dtype=tf.float32,
+            tf.get_variable("w_parser_s", [2 * self.config.dim_str, 2 * self.config.dim_str], dtype=tf.float64,
                             initializer=self.xavier_init)
-            tf.get_variable("bias_parser_p", [2 * self.config.dim_str], dtype=tf.float32,
+            tf.get_variable("bias_parser_p", [2 * self.config.dim_str], dtype=tf.float64,
                             initializer=self.xavier_init)
-            tf.get_variable("bias_parser_c", [2 * self.config.dim_str], dtype=tf.float32,
+            tf.get_variable("bias_parser_c", [2 * self.config.dim_str], dtype=tf.float64,
                             initializer=self.xavier_init)
-            tf.get_variable("w_parser_root", [2 * self.config.dim_str, 1], dtype=tf.float32,
+            tf.get_variable("w_parser_root", [2 * self.config.dim_str, 1], dtype=tf.float64,
                             initializer=self.xavier_init)
         with tf.variable_scope("Structure/sent"):
             tf.get_variable("w_parser_p", [2 * self.config.dim_str, 2 * self.config.dim_str],
-                            dtype=tf.float32,
+                            dtype=tf.float64,
                             initializer=self.xavier_init)
             tf.get_variable("w_parser_c", [2 * self.config.dim_str, 2 * self.config.dim_str],
-                            dtype=tf.float32,
+                            dtype=tf.float64,
                             initializer=self.xavier_init)
-            tf.get_variable("bias_parser_p", [2 * self.config.dim_str], dtype=tf.float32,
+            tf.get_variable("bias_parser_p", [2 * self.config.dim_str], dtype=tf.float64,
                             initializer=self.xavier_init)
-            tf.get_variable("bias_parser_c", [2 * self.config.dim_str], dtype=tf.float32,
+            tf.get_variable("bias_parser_c", [2 * self.config.dim_str], dtype=tf.float64,
                             initializer=self.xavier_init)
 
-            tf.get_variable("w_parser_s", [2 * self.config.dim_str, 2 * self.config.dim_str], dtype=tf.float32,
+            tf.get_variable("w_parser_s", [2 * self.config.dim_str, 2 * self.config.dim_str], dtype=tf.float64,
                             initializer=self.xavier_init)
-            tf.get_variable("w_parser_root", [2 * self.config.dim_str, 1], dtype=tf.float32,
+            tf.get_variable("w_parser_root", [2 * self.config.dim_str, 1], dtype=tf.float64,
                             initializer=self.xavier_init)
 
         sent_l = self.t_variables['sent_l']
@@ -138,12 +138,10 @@ class StructureModel():
 
         mask_tokens = self.t_variables['mask_tokens'][:, :max_doc_l, :max_sent_l]
         mask_sents = self.t_variables['mask_sents'][:, :max_doc_l]  # [batch_size, doc_l]
-        # [_, _, _, rnn_size] = tokens_input.get_shape().as_list()
-        tokens_input_do = tf.reshape(tokens_input, [batch_l * max_doc_l, max_sent_l, self.config.d_embed])
 
+        tokens_input_do = tf.reshape(tokens_input, [batch_l * max_doc_l, max_sent_l, self.config.d_embed])
         sent_l = tf.reshape(sent_l, [batch_l * max_doc_l])
         mask_tokens = tf.reshape(mask_tokens, [batch_l * max_doc_l, -1])
-
         tokens_output, _ = dynamicBiRNN(tokens_input_do, sent_l, n_hidden=self.config.dim_hidden, xavier_init=self.xavier_init,
                                         cell_type=self.config.rnn_cell, cell_name='Model/sent')
         tokens_sem = tf.concat([tokens_output[0][:,:,:self.config.dim_sem], tokens_output[1][:,:,:self.config.dim_sem]], 2)
@@ -152,11 +150,11 @@ class StructureModel():
         if self.config.skip_sent_attention:
             tokens_output = LReLu(tf.tensordot(tf.concat([tokens_sem, tokens_input_do], 2), w_comb_s, [[2], [0]]) + b_comb_s)
         else:
-            temp1 = tf.zeros([batch_l * max_doc_l, max_sent_l,1], tf.float32)
-            temp2 = tf.zeros([batch_l * max_doc_l,1,max_sent_l], tf.float32)
+            temp1 = tf.zeros([batch_l * max_doc_l, max_sent_l,1], tf.float64)
+            temp2 = tf.zeros([batch_l * max_doc_l,1,max_sent_l], tf.float64)
 
-            mask1 = tf.ones([batch_l * max_doc_l, max_sent_l, max_sent_l-1], tf.float32)
-            mask2 = tf.ones([batch_l * max_doc_l, max_sent_l-1, max_sent_l], tf.float32)
+            mask1 = tf.ones([batch_l * max_doc_l, max_sent_l, max_sent_l-1], tf.float64)
+            mask2 = tf.ones([batch_l * max_doc_l, max_sent_l-1, max_sent_l], tf.float64)
             mask1 = tf.concat([temp1,mask1],2)
             mask2 = tf.concat([temp2,mask2],1)
 
@@ -168,11 +166,11 @@ class StructureModel():
 
             # create mask for setting the padded diagonals to 1
             mask_diags = tf.matrix_diag_part(mask_tokens_mult)
-            mask_diags_invert = tf.cast(tf.logical_not(tf.cast(mask_diags, tf.bool)), tf.float32)
-            zero_matrix = tf.zeros([batch_l * max_doc_l, max_sent_l, max_sent_l], tf.float32)
+            mask_diags_invert = tf.cast(tf.logical_not(tf.cast(mask_diags, tf.bool)), tf.float64)
+            zero_matrix = tf.zeros([batch_l * max_doc_l, max_sent_l, max_sent_l], tf.float64)
             mask_tokens_add = tf.matrix_set_diag(zero_matrix, mask_diags_invert)
 
-            str_scores_s_, _, LL_tokens, LL_tokens_unmasked = get_structure('sent', tokens_str, max_sent_l, mask1, mask2, mask_tokens_mult, mask_tokens_add)  # batch_l,  sent_l+1, sent_l
+            str_scores_s_, _, LL_tokens, LL_tokens_masked = get_structure('sent', tokens_str, mask1, mask2, mask_tokens_mult, mask_tokens_add,  tf.expand_dims(mask_tokens,2))  # batch_l,  sent_l+1, sent_l
             str_scores_s = tf.matrix_transpose(str_scores_s_)  # soft parent
             tokens_sem_root = tf.concat([tf.tile(embeddings_root_s, [batch_l * max_doc_l, 1, 1]), tokens_sem], 1)
             tokens_output_ = tf.matmul(str_scores_s, tokens_sem_root)
@@ -183,7 +181,7 @@ class StructureModel():
             tokens_output = tf.reduce_sum(tokens_output, 1)
         elif (self.config.sent_attention == 'mean'):
             tokens_output = tokens_output * tf.expand_dims(mask_tokens,2)
-            tokens_output = tf.reduce_sum(tokens_output, 1)/tf.expand_dims(tf.cast(sent_l,tf.float32),1)
+            tokens_output = tf.reduce_sum(tokens_output, 1)/tf.expand_dims(tf.cast(sent_l,tf.float64),1)
         elif (self.config.sent_attention == 'max'):
             tokens_output = tokens_output + tf.expand_dims((mask_tokens-1)*999,2)
             tokens_output = tf.reduce_max(tokens_output, 1)
@@ -221,11 +219,11 @@ class StructureModel():
 
             # create mask for setting the padded diagonals to 1
             mask_sents_diags = tf.matrix_diag_part(mask_sents_mult)
-            mask_sents_diags_invert = tf.cast(tf.logical_not(tf.cast(mask_sents_diags, tf.bool)), tf.float32)
-            zero_matrix_sents = tf.zeros([batch_l, max_doc_l, max_doc_l], tf.float32)
+            mask_sents_diags_invert = tf.cast(tf.logical_not(tf.cast(mask_sents_diags, tf.bool)), tf.float64)
+            zero_matrix_sents = tf.zeros([batch_l, max_doc_l, max_doc_l], tf.float64)
             mask_sents_add = tf.matrix_set_diag(zero_matrix_sents, mask_sents_diags_invert)
 
-            str_scores_, str_scores_no_root, LL_sents, LL_sents_unmasked = get_structure('doc', sents_str, max_doc_l, self.t_variables['mask_parser_1'], self.t_variables['mask_parser_2'], mask_sents_mult, mask_sents_add)  # [batch_size, doc_l+1, doc_l]
+            str_scores_, str_scores_no_root, LL_sents, LL_sents_masked = get_structure('doc', sents_str, self.t_variables['mask_parser_1'], self.t_variables['mask_parser_2'], mask_sents_mult, mask_sents_add, tf.expand_dims(mask_sents,2))  # [batch_size, doc_l+1, doc_l]
             str_scores = tf.matrix_transpose(str_scores_)
             self.str_scores = str_scores  # shape is [batch_size, doc_l, doc_l+1]
 
@@ -253,13 +251,13 @@ class StructureModel():
             sents_output = tf.reduce_sum(sents_output, 1)  # [batch_size, dim_sem*2]
         elif (self.config.doc_attention == 'mean'):
             sents_output = sents_output * tf.expand_dims(mask_sents, 2)
-            sents_output = tf.reduce_sum(sents_output, 1)/tf.expand_dims(tf.cast(doc_l,tf.float32),1)
+            sents_output = tf.reduce_sum(sents_output, 1)/tf.expand_dims(tf.cast(doc_l,tf.float64),1)
         elif (self.config.doc_attention == 'max'):
             sents_output = sents_output + tf.expand_dims((mask_sents-1)*999,2)
             sents_output = tf.reduce_max(sents_output, 1)
         elif (self.config.doc_attention == 'weighted_sum'):
             sents_weighted = sents_output * tf.expand_dims(str_scores[:,:,0], 2)
-            sents_output = sents_weighted * tf.expand_dims(mask_sents, 2) # unmask
+            sents_output = sents_weighted * tf.expand_dims(mask_sents, 2)  # apply mask
             sents_output = tf.reduce_sum(sents_output, 1)
 
         final_output = MLP(sents_output, 'output', self.t_variables['keep_prob'], self.config.seed, self.xavier_init)

neural.py
@@ -8,19 +8,19 @@ def LReLu(x, leak=0.01):
 
 def dynamicBiRNN(input, seqlen, n_hidden, xavier_init, cell_type, cell_name=''):
     batch_size = tf.shape(input)[0]
-    with tf.variable_scope(cell_name + 'fw', initializer=xavier_init, dtype = tf.float32):
+    with tf.variable_scope(cell_name + 'fw', initializer=xavier_init, dtype = tf.float64):
         if(cell_type == 'gru'):
             fw_cell = tf.contrib.rnn.GRUCell(n_hidden)
         elif(cell_type == 'lstm'):
             fw_cell = tf.contrib.rnn.LSTMCell(n_hidden)
 
-        fw_initial_state = fw_cell.zero_state(batch_size, tf.float32)
-    with tf.variable_scope(cell_name + 'bw', initializer=xavier_init, dtype = tf.float32):
+        fw_initial_state = fw_cell.zero_state(batch_size, tf.float64)
+    with tf.variable_scope(cell_name + 'bw', initializer=xavier_init, dtype = tf.float64):
         if(cell_type == 'gru'):
             bw_cell = tf.contrib.rnn.GRUCell(n_hidden)
         elif(cell_type == 'lstm'):
             bw_cell = tf.contrib.rnn.LSTMCell(n_hidden)
-        bw_initial_state = bw_cell.zero_state(batch_size, tf.float32)
+        bw_initial_state = bw_cell.zero_state(batch_size, tf.float64)
     with tf.variable_scope(cell_name):
         outputs, output_states = tf.nn.bidirectional_dynamic_rnn(fw_cell, bw_cell, input,
                                                                  initial_state_fw=fw_initial_state,
@@ -33,16 +33,16 @@ def MLP(input, vname, keep_prob, seed, xavier_init):
     dim_input = input.shape[1]
     with tf.variable_scope("Model"):
         w1 = tf.get_variable("w1_"+vname,[dim_input, dim_input],
-                            dtype=tf.float32,
+                            dtype=tf.float64,
                             initializer=xavier_init)
         b1 = tf.get_variable("bias1_" + vname,[dim_input],
-                            dtype=tf.float32,
+                            dtype=tf.float64,
                             initializer=tf.constant_initializer())
         w2 = tf.get_variable("w2_" + vname,[dim_input, dim_input],
-                            dtype=tf.float32,
+                            dtype=tf.float64,
                             initializer=xavier_init)
         b2 = tf.get_variable("bias2_" + vname,[dim_input],
-                            dtype=tf.float32,
+                            dtype=tf.float64,
                              initializer=tf.constant_initializer())
     input = tf.nn.dropout(input,keep_prob,seed=seed)
     h1 = LReLu(tf.matmul(input, w1) + b1)
@@ -51,10 +51,10 @@ def MLP(input, vname, keep_prob, seed, xavier_init):
     return h2
 
 
-def get_structure(name, input, max_l, mask_parser_1, mask_parser_2, mask_multiply, mask_add):
-    def _getDep(input, mask1, mask2, mask_multiply, mask_add):
+def get_structure(name, input, mask_parser_1, mask_parser_2, mask_multiply, mask_add, mask):
+    def _getDep(input, mask1, mask2, mask_multiply, mask_add, mask):
         #input: batch_l, sent_l, rnn_size
-        with tf.variable_scope("Structure/"+name, reuse=True, dtype=tf.float32):
+        with tf.variable_scope("Structure/"+name, reuse=True, dtype=tf.float64):
             w_parser_p = tf.get_variable("w_parser_p")
             w_parser_c = tf.get_variable("w_parser_c")
             b_parser_p = tf.get_variable("bias_parser_p")
@@ -64,42 +64,40 @@ def get_structure(name, input, max_l, mask_parser_1, mask_parser_2, mask_multipl
             w_parser_root = tf.get_variable("w_parser_root")
 
         parent = tf.tanh(tf.tensordot(input, w_parser_p, [[2], [0]]) + b_parser_p)
+        parent_masked = parent * mask
         child = tf.tanh(tf.tensordot(input, w_parser_c, [[2], [0]])+b_parser_c)
-        temp = tf.tensordot(parent,w_parser_s,[[-1],[0]])
-        raw_scores_words_ = tf.matmul(temp,tf.matrix_transpose(child))
+        child_masked = child * mask
+        temp = tf.tensordot(parent_masked,w_parser_s,[[-1],[0]])
+        raw_scores_words_ = tf.matmul(temp,tf.matrix_transpose(child_masked))
 
         # raw_scores_words_ = tf.squeeze(tf.tensordot(rep, w_parser_s, [[3], [0]]) , [3])
-        raw_scores_root_ = tf.squeeze(tf.tensordot(input, w_parser_root, [[2], [0]]) , [2])
+        raw_scores_root_ = tf.squeeze(tf.tensordot(input, w_parser_root, [[2], [0]]), [2])
         raw_scores_words = tf.exp(raw_scores_words_)
         raw_scores_root = tf.exp(raw_scores_root_)
         tmp = tf.zeros_like(raw_scores_words[:,:,0])
         raw_scores_words = tf.matrix_set_diag(raw_scores_words,tmp)
 
-        str_scores, str_scores_no_root, LL, LL_unmasked = _getMatrixTree(raw_scores_root, raw_scores_words, mask1, mask2, mask_multiply, mask_add)
-        return str_scores, str_scores_no_root, LL, LL_unmasked
+        str_scores, str_scores_no_root, LL, LL_masked = _getMatrixTree(raw_scores_root, raw_scores_words, mask1, mask2, mask_multiply, mask_add)
+        return str_scores, str_scores_no_root, LL, LL_masked
 
     def _getMatrixTree(r, A, mask1, mask2, mask_multiply, mask_add):
-        L = tf.reduce_sum(A, 1)
-        L = tf.matrix_diag(L)
-        L = L - A
-        LL = L[:, 1:, :]
-        LL = tf.concat([tf.expand_dims(r, [1]), LL], 1)
-        #mask_ll = tf.reshape(mask_ll, [mask_ll.shape[0]*mask_ll.shape[1], mask_ll.shape[2]])
-        LL_unmasked = mask_multiply * LL
-        LL_unmasked = LL_unmasked + mask_add
-        #c=LL[1][1]
-        #LL_norm = tf.divide(LL, c)
-        #LL_norm_inv = tf.matrix_inverse(LL_norm)
-        #LL_inv = tf.multiply(LL_norm_inv, c)
-        LL_inv = tf.matrix_inverse(LL_unmasked)  # batch_l, doc_l, doc_l
+        A_masked = A * mask_multiply
+        L_reduce = tf.reduce_sum(A_masked, 1)
+        L_diag = tf.matrix_diag(L_reduce)
+        L_minus = L_diag - A_masked
+        LL_diag = L_minus[:, 1:, :]
+        LL = tf.concat([tf.expand_dims(r, [1]), LL_diag], 1)
+        LL_masked = mask_multiply * LL
+        LL_masked = LL_masked + mask_add
+        LL_inv = tf.matrix_inverse(LL_masked)  # batch_l, doc_l, doc_l
         #LL_inv = tf.matrix_inverse(LL)  # batch_l, doc_l, doc_l
         d0 = tf.multiply(r, LL_inv[:, :, 0])  # root
         LL_inv_diag = tf.expand_dims(tf.matrix_diag_part(LL_inv), 2)
-        tmp1 = tf.matrix_transpose(tf.multiply(tf.matrix_transpose(A), LL_inv_diag))
-        tmp2 = tf.multiply(A, tf.matrix_transpose(LL_inv))
+        tmp1 = tf.matrix_transpose(tf.multiply(tf.matrix_transpose(A_masked), LL_inv_diag))
+        tmp2 = tf.multiply(A_masked, tf.matrix_transpose(LL_inv))
         d_no_root = mask1 * tmp1 - mask2 * tmp2
         d = tf.concat([tf.expand_dims(d0,[1]), d_no_root], 1)  # add column at beginning for root
-        return d, d_no_root, LL, LL_unmasked
+        return d, d_no_root, LL, LL_masked
 
-    str_scores, str_scores_no_root, LL, LL_unmasked = _getDep(input, mask_parser_1, mask_parser_2, mask_multiply, mask_add)
-    return str_scores, str_scores_no_root, LL, LL_unmasked
+    str_scores, str_scores_no_root, LL, LL_masked = _getDep(input, mask_parser_1, mask_parser_2, mask_multiply, mask_add, mask)
+    return str_scores, str_scores_no_root, LL, LL_masked

predictor.py
@@ -28,7 +28,7 @@ class InMemoryClient:
         input_tensor_names = [meta_graph_def_sig.inputs[k].name for k in input_tensor_keys]
 
         self.t_variables = {key: name for key, name in zip(input_tensor_keys, input_tensor_names)}
-
+     
         self.final_output = meta_graph_def_sig.outputs['output'].name
         self.str_scores = meta_graph_def_sig.outputs['str_scores'].name
 
@@ -105,8 +105,8 @@ class InMemoryClient:
         sent_l_matrix = np.zeros([batch_size, max_doc_l], np.int32)
         gold_matrix = np.zeros([batch_size], np.int32)
         id_matrix = np.zeros([batch_size], np.int32)
-        mask_tokens_matrix = np.ones([batch_size, max_doc_l, max_sent_l], np.float32)
-        mask_sents_matrix = np.ones([batch_size, max_doc_l], np.float32)
+        mask_tokens_matrix = np.ones([batch_size, max_doc_l, max_sent_l], np.float64)
+        mask_sents_matrix = np.ones([batch_size, max_doc_l], np.float64)
         for i, instance in enumerate(batch):
             n_sents = len(instance.token_idxs)
             gold_matrix[i] = instance.goldLabel
@@ -117,8 +117,8 @@ class InMemoryClient:
                 sent_l_matrix[i, j] = len(sent)
             mask_sents_matrix[i, n_sents:] = 0
 
-        mask_parser_1 = np.ones([batch_size, max_doc_l, max_doc_l], np.float32)
-        mask_parser_2 = np.ones([batch_size, max_doc_l, max_doc_l], np.float32)
+        mask_parser_1 = np.ones([batch_size, max_doc_l, max_doc_l], np.float64)
+        mask_parser_2 = np.ones([batch_size, max_doc_l, max_doc_l], np.float64)
         mask_parser_1[:, :, 0] = 0  # zero out 1st column for each doc
         mask_parser_2[:, 0, :] = 0  # zero out 1st row for each doc
 